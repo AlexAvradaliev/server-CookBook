896ede5474452a7f7aeeac4c3a92a046a08730e0
aleksandarAvr <avradaliev@gmail.com>
2022-06-03 18:26:42 +0300

    add middleweare guards

---
+++ middleweare/guards.js
@@ -0,0 +1,27 @@
+function isAuth(){
+    return (req,res,next) =>{
+        if(req.users){
+            next();
+        } else {
+            res.status(401).json({message: 'Please login!'});
+        };
+    };
+};
+
+function isGuest(){
+    return (req,res,next) =>{
+        
+        if(!req.users){
+            next();
+        } else {
+            res.status(400).json({message: 'You are already login!'});
+        };
+    };
+};
+
+
+
+module.exports = {
+    isAuth,
+    isGuest,
+};
