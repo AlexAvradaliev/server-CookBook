e62e876ce8bb956da402af1689f8d911f07ba233
aleksandarAvr <avradaliev@gmail.com>
2022-06-03 21:29:17 +0300

    create recipe model

---
+++ models/Recipe.js
@@ -0,0 +1,46 @@
+const { model, Schema, Types: {ObjectId} } = require('mongoose');
+
+
+const recipeSchema = new Schema(
+	{
+		_ownerId: {
+			type:ObjectId,
+			ref: 'User',
+		},
+		name: {
+			type: String,
+		},
+		description: {
+			type: String,
+		},
+		images: {
+			type: [String],
+			default: ['/images/no-image.jpg'],
+		},
+		cuisine: {
+			type: String,
+		},
+		level: {
+			type: String,
+		},
+		prepTime: {
+			type: String,
+		},
+		cookTime: {
+			type: String,
+		},
+		ingredients: {
+			type: [String],
+		},
+		groups: {
+			type: [String],
+		},
+		steps: {
+			type: [String],
+		},
+	},
+	{ timestamps: true }
+);
+
+const Recipe = model('Recipe', recipeSchema);
+module.exports = Recipe;
