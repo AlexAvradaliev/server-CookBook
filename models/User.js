d58bfe4e77c7ab9c4681f98226eb0c9ddcdfcc5a
aleksandarAvr <avradaliev@gmail.com>
2022-06-03 15:48:35 +0300

    create user model

---
+++ models/User.js
@@ -0,0 +1,35 @@
+const { model, Schema } = require('mongoose');
+
+
+const userSchema = new Schema({
+    firstName: {
+        type: String,
+    },
+    lastName: {
+        type: String,
+    },
+    email: {
+        type: String,
+        unique: true,
+    },
+    hashedPassword: {
+        type: String,
+    },
+    photo: {
+        type: String,
+        default: '/images/user.webp'
+    }
+},
+    { timestamps: true }
+);
+
+
+userSchema.index({ email: 1 }, {
+    collation: {
+        locale: 'en',
+        strength: 1
+    }
+})
+
+const User = model('User', userSchema);
+module.exports = User;
