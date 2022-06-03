2cada0286677ae6f5b52796c077043d960960eaa
aleksandarAvr <avradaliev@gmail.com>
2022-06-03 18:41:10 +0300

    add middleweare validation register

---
+++ middleweare/validation/user.js
@@ -0,0 +1,32 @@
+const { body } = require('express-validator');
+
+function validateRegister() {
+    return (req, res) => {
+        body('firstName')
+            .trim()
+        body('lastName')
+            .trim()
+        body('email')
+            .trim()
+            .normalizeEmail(),
+        body('password')
+                .trim(),
+        body('repassword')
+                .trim(),
+        body('firstName')
+                .isLength({ min: 1, max: 12 }).withMessage('first name can be between 1-12 char'),
+        body('lastName')
+                .isLength({ min: 1, max: 12 }).withMessage('first name can be between 1-12 char'),
+        body('email')
+                .isEmail().withMessage('email is not valid'),
+        body('password')
+                .isLength({ min: 6 }).withMessage('password can be 6 char'),
+        body('repassword')
+                .custom(async (value, { req }) => {
+                    if (value != req.body.password) {
+                        throw new Error('passwords don\'t match');
+                    };
+                });
+    }
+};
+
