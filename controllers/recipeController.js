026e60a54f5294dc24719d6ca0b62a7d3b03f64d
aleksandarAvr <avradaliev@gmail.com>
2022-06-04 12:06:42 +0300

    edit recipe in controller

--- controllers/recipeController.js
+++ controllers/recipeController.js
@@ -7,7 +7,7 @@ router.get(`/`, async (req, res) => {
     try {
         const queryInfo = req.query;
         let data = await recipe.getAll(queryInfo);
-        res.json(data); 
+        res.json(data);
     } catch (err) {
         console.log(err);
     };
@@ -21,15 +21,15 @@ router.post(`/`, async (req, res) => {
             groups: req.body.groups,
             steps: req.body.steps,
             name: req.body.name,
-            description:req.body.description,
+            description: req.body.description,
             cuisine: req.body.cuisine,
             level: req.body.level,
             cookTime: req.body.cookTime,
             prepTime: req.body.prepTime,
             _ownerId: req.users._id,
-          };
-            const result = await recipe.create(data);
-            return res.status(201).json(result);
+        };
+        const result = await recipe.create(data);
+        return res.status(201).json(result);
 
     } catch (err) {
         console.log(err);
@@ -50,6 +50,30 @@ router.get('/:id', async (req, res) => {
     try {
         const data = await recipe.getOneById(req.params.id);
         res.json(data);
+
+    } catch (err) {
+        console.log(err);
+    };
+});
+
+router.put('/:id', isAuth(), async (req, res) => {
+    try {
+        const userId = req.users._id;
+        const recipeId = req.params.id;
+        const data = {
+            images: req.body.images,
+            ingredients: req.body.ingredients,
+            groups: req.body.groups,
+            steps: req.body.steps,
+            name: req.body.name,
+            description: req.body.description,
+            cuisine: req.body.cuisine,
+            level: req.body.level,
+            cookTime: req.body.cookTime,
+            prepTime: req.body.prepTime,
+        };
+        const result = await recipe.update(recipeId, data, userId);
+        return res.json(result);
 
     } catch (err) {
         console.log(err);
