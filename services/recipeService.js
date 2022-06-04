877aea9e0fb797e4e986cf23e0d99cc2eee250ed
aleksandarAvr <avradaliev@gmail.com>
2022-06-04 11:12:46 +0300

    add function get all recipe with pagination

--- services/recipeService.js
+++ services/recipeService.js
@@ -19,7 +19,68 @@ async function getOneById(id) {
     } catch (err) {
         console.log(err);
     };
+};
 
+async function getAll(queryInfo) {
+    let query;
+    const reqQuery = { ...queryInfo };
+    let category = {};
+    const searchName = queryInfo.search;
+    const currentPage = queryInfo.page;
+
+    //  Searching
+    if (searchName) {
+        category = {
+            name: {
+                $regex: searchName,
+                $options: "i",
+            },
+        };
+    } else if (reqQuery.hasOwnProperty('cuisine')) {
+        category = {
+            'cuisine': reqQuery.cuisine,
+            'name': { $regex: searchName, $options: 'i' }
+        };
+    } else if (reqQuery.hasOwnProperty('groups')) {
+        category = {
+            'groups': reqQuery.groups,
+            'name': { $regex: searchName, $options: 'i' }
+        };
+    };
+
+    query = Recipe.find(category);
+    query = query.sort("-createdAt");
+
+    //  Pagination
+    const pageNumber = Number(currentPage) || 1;
+    const pageSize = 16;
+
+    const skip = (pageNumber - 1) * pageSize;
+    const count = await Recipe.countDocuments(category);
+
+    query = query.skip(skip).limit(pageSize);
+
+    // Pagination result
+    const paginationObj = {
+        page: pageNumber,
+        pages: Math.ceil(count / pageSize),
+    };
+
+    if (pageNumber * pageSize < count) {
+        paginationObj.nextPage = pageNumber + 1;
+    };
+
+    if (skip > 0) {
+        paginationObj.previousPage = pageNumber - 1;
+    };
+
+    const recipes = await query
+        .populate("_ownerId", "firstName lastName photo");
+    return ({
+        count: recipes.length,
+        pagination: paginationObj,
+        recipes: recipes,
+    });
 };
 
 module.exports = {
